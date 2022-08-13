8528a167a1125d9440b64b12dd73562165d08a68
BogdanKorolev <bkorolevjob@gmail.com>
2022-08-13 20:59:48 +0300

    Task41 code written

---
+++ Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+Задача 41:
+Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+*/
+int sum = 0;
+int m = new Random().Next(4,6);
+int size = m;
+int[] array = new int[m];
+Console.WriteLine($"Введите следующее количество чисел: {m}");
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine($"Сумма положительных чисел = {sum}");
